4a2dc22a8b574644ad7782e29817a7823674d7cf
Awwad <msa492@gmail.com>
2026-05-20 14:16:52 +0200

    finished part 1

---
+++ mobile/ios/mobile/Notification.m
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(Notification, NSObject)
+
+RCT_EXTERN_METHOD(requestPermissions)
+RCT_EXTERN_METHOD(showNotification:(NSString *)title
+                  location:(NSString *)body)
+
++ (BOOL)requiresMainQueueSetup
+{
+  return YES;
+}
+
+@end
